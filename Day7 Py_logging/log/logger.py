ec5f62118b7c096500f16ee31ecb45e1f6e2aafb
Sachin Mugade <sachinmugade8797@gmail.com>
2025-09-08 19:57:26 +0530

    Day7 work add

---
+++ Day7 Py_logging/log/logger.py
@@ -0,0 +1,10 @@
+
+## configure the basic logging settings
+logging.basicConfig(level=logging.DEBUG)
+
+## log messages
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
